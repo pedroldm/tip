a5eba26ee4c64b07d775d14469d045f5ea1d5ee3
Pedro Lucas Damasceno <pedroldmf@gmail.com>
2025-05-06 20:29:36 -0300

    Updating parameters

--- src/io/instances_executor.py
+++ src/io/instances_executor.py
@@ -9,21 +9,23 @@ import tqdm
 
 class InstancesExecutor:
     params = {
-        "p": 300, # [200, 300, 400, 500]
-        "pe": 0.10, # [0.05, 0.10, 0.15]
-        "pm": 0.20, # [0.10, 0.20, 0.30]
-        "rhoe": 0.55, # [0.50, 0.55, 0.60, 0.65]
+        "p": 400, # [200, 300, 400, 500]
+        "pe": 0.15, # [0.05, 0.10, 0.15]
+        "pm": 0.10, # [0.10, 0.20, 0.30]
+        "rhoe": 0.5, # [0.50, 0.55, 0.60, 0.65]
         "K": 1, # [1]
-        "MAXT": 1, # [-]
+        "MAXT": 16, # [-]
         "X_INTVL": 1, # [-]
         "X_NUMBER": 0, # []
-        "MAX_GENS": 500, # [5000]
+        "MAX_GENS": 700, # [5000]
+        "MAX_GENS_WITHOUT_IMPROVEMENT": 100,
         "irace": "false",
-        "lsNonEliteApplicationPercentage": 0.05,
-        "lsEliteApplicationPercentage": 0.25,
-        "lsCoveragePercentage": 1.0,
+        "lsNonEliteAplicationPercentage": 0.05,
+        "lsEliteApplicationPercentage": 0.5,
+        "lsCoveragePercentage": 0.75,
         "timeLimit": 120,
         "useVND": "true",
+        "PM_INCREASE_FACTOR": 0.10
     }
 
     def __init__(self, params: dict) :
@@ -42,7 +44,7 @@ class InstancesExecutor:
         return [os.path.join(root, file) for root, _, files in os.walk(directory) for file in files]
     
     def run(self):
-        args = ['/home/pedro/tip/main_prd']
+        args = ['/home/pedro/Pessoal/MSc/tip/main_prd']
         for p, v in self.params.items():
             args.append(f"--{p}={v}")
             
@@ -50,7 +52,7 @@ class InstancesExecutor:
         return json.loads(result.stdout)
     
     def run_all(self, runs: int = 1, instances_subset = []):
-        instances = self.scan_files_in_subdirs("/home/pedro/tip/instances")
+        instances = self.scan_files_in_subdirs("/home/pedro/Pessoal/MSc/tip/instances")
         if instances_subset:
             instances = [i for i in instances if os.path.basename(os.path.dirname(i)) in instances_subset]
 
